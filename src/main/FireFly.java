5fe087d6d4e472a12a6a1a4be7806a74cb988cac
German <German@German-PC>
2016-05-12 17:11:16 -0300

    Corrección en generación de la población e instanciación de luciérnagas

--- src/main/FireFly.java
+++ src/main/FireFly.java
@@ -21,12 +21,9 @@ public class FireFly {
 
 //Muestra todos los caracteres distintos ingresados
 	    //verLetras(letras);
-		    
-//Esto tiene que ir dentro de la generacion de la poblacion de luciernagas para poder inicializar a cada una
-		char[] vector = generador(letras);
-		
+		    		
 //Creación de la poblacion de luciernagas		
-		Luciernaga[] swarm = poblacion(3);
+		Luciernaga[] swarm = poblacion(3,letras);
 
 		
 //Comparar luciernagas teniendo en cuenta el brillo y el atractivo de cada una
@@ -70,31 +67,42 @@ public class FireFly {
 	}
 //Mapeador de Caracteres con Numeros ----> Para generar la aleatoriedad de las luciernagas
 	public static char[] generador(HashMap<Character, Integer> checkSum){
+		HashMap<Integer, Character> mapeador = new HashMap<Integer,Character>();
 		char[] letras = new char[10];
 		Random valor = new Random();
 	    Iterator it = checkSum.entrySet().iterator();
+	    System.out.println("Esta es una Luciernaga");
 	    while (it.hasNext()) {
 	    	Map.Entry e = (Map.Entry)it.next();
-	    	System.out.println(e.getKey() + " " + e.getValue());
 			int valorcito = valor.nextInt(10);
-			letras[valorcito] = (char)e.getKey();
-	    }  		 
-		//Solo para ver como se fueron asignador las letras a las posiciones
+			while(mapeador.containsKey(valorcito)){
+				valorcito = valor.nextInt(10);
+			}
+			mapeador.put(valorcito, (char)e.getKey());			
+	    }
+	    
+	    Iterator it2 = mapeador.entrySet().iterator();
+	    while (it2.hasNext()){
+	    	Map.Entry e2 = (Map.Entry)it2.next();
+	    	letras[(int)e2.getKey()] = (char)e2.getValue(); 
+	    }
+//Solo para ver como se fueron asignador las letras a las posiciones
 	    int i = 0;
 		for(char unChar:letras){
 			System.out.println(unChar + " " + i );
 			i = i + 1;
 		}
-		
 		return letras; 
 	}
 	
 //Generación de la Población de Luciernagas
-	public static Luciernaga[] poblacion(int habitantes){
+	public static Luciernaga[] poblacion(int habitantes, HashMap<Character, Integer> letras ){
 		Luciernaga[] enjambre = new Luciernaga[habitantes];
 		for (int h =0; h < enjambre.length; h++) {
 			String nombre = "luciernaga" + h;
-			Luciernaga unaLuciernaga = new Luciernaga(nombre);
+			//Esto tiene que ir dentro de la generacion de la poblacion de luciernagas para poder inicializar a cada una
+			char[] vector = generador(letras);
+			Luciernaga unaLuciernaga = new Luciernaga(nombre,vector);
 			enjambre[h] = unaLuciernaga;
 		}
 		return enjambre;
--- src/model/Luciernaga.java
+++ src/model/Luciernaga.java
@@ -5,20 +5,30 @@ import interfaces.IComportamiento;
 public class Luciernaga implements IComportamiento{
 
 	public String id;
-	
+	public char[] elementos;
 	public Luciernaga(){}
 	
-	public Luciernaga(String unId){
+	public Luciernaga(String unId, char[] unElemento){
 		this.id = unId;
+		this.elementos = unElemento;
 	}
 	
 	public void setId(String unId){
 		this.id = unId;
 	}
 	
-	public String getId(String unId){
+	public String getId(){
 		return id;
 	}
+
+	public void setElementos(char[] unElemento){
+		this.elementos = unElemento;
+	}
+	
+	public char[] getElementos(){
+		return elementos;
+	}
+	
 	
 	@Override
 	public int intensidad() {
